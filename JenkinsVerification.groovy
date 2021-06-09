245626b9f265cf97468c444e835deaea7a49602a
asjrosabal <asjrosabal@gmail.com>
2021-06-09 16:17:44 -0400

    Update JenkinsVerification.groovy
    cambios jenkins

--- JenkinsVerification.groovy
+++ JenkinsVerification.groovy
@@ -3,7 +3,12 @@ node{
     println "build"
   }
   stage('SAST'){
-    println "sast"
+    sh ''' 
+    ./gradlew sonarqube \
+    -Dsonar.projectKey=github2 \
+    -Dsonar.host.url=http://localhost:9000 \
+    -Dsonar.login=1890805ea65026d01d5d91a5bdc52f66c42659bb
+    '''
   }
    stage('pruebas funcionales'){
     println "QA"
