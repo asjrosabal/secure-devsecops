a977566e4290bac5fd8d198e86a548cbe940185d
asjrosabal <asjrosabal@gmail.com>
2021-07-26 10:24:15 -0400

    Update sca.groovy

--- sca.groovy
+++ sca.groovy
@@ -1,9 +1,22 @@
 node{
-  stage('build'){
-    println "build"
+  stage('Checkout'){
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/asjrosabal/ms-gradle']]])
   }
-  stage('SAST'){
-    println "QA"
+  stage('SCA'){
+    println 'ANTES'
+    sh 'cat build.gradle'
+    def readContent = readFile 'build.gradle'
+    
+    def task = '''
+    task libraries(type: Copy) {
+      from configurations.runtime
+      into "lib"
+    }
+    '''
+    writeFile file: 'build.gradle', text: readContent+"\r\n${task}"
+    
+    println 'DEPSUES'
+    sh 'cat build.gradle'
     
   }
    stage('pruebas funcionales'){
